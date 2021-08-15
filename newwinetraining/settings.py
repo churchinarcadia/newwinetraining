e9a994e6bee4fc32415ce26c4035c2ddb8f85f9f
David D Wang <duendwang@users.noreply.github.com>
2021-08-14 18:28:36 -0700

    changes to settings

--- newwinetraining/settings.py
+++ newwinetraining/settings.py
@@ -58,7 +58,7 @@ else:
 
 MIDDLEWARE = [
     'iommi.live_edit.Middleware',
-    
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    
     'iommi.sql_trace.Middleware',
     'iommi.profiling.Middleware',
     'iommi.middleware', #must be last in the list
