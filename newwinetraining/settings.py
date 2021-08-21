febb4b3e200887d7a36c978a731a7c27e83c0f8b
David D Wang <duendwang@users.noreply.github.com>
2021-08-21 02:33:54 -0700

    Minor changes to settings

--- newwinetraining/settings.py
+++ newwinetraining/settings.py
@@ -30,9 +30,10 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
+#DEBUG = False
 
-#ALLOWED_HOSTS = env('ALLOWED_HOSTS')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+#ALLOWED_HOSTS = []
 
 # Application definition
 
